121cd4e394b5a3c6b3817954b373310505703531
Samuel Witke <witkesam@gmail.com>
2017-06-22 11:25:53 -0400

    modified:   sketch.js

--- sketch.js
+++ sketch.js
@@ -12,19 +12,19 @@ function heruristic(a,b){
 	return d;
 }
 
-var cols = 50;
-var rows = 50;
-
+var cols = 20;
+var rows = 20;
 var grid = new Array(cols)
-
 var openSet = [];
 var closedSet = [];
 var start;
 var end;
-
 var w;
 var h;
 var path = [];
+var	start = -1; 
+var	end = -1;
+var pointCount = 2;
 
 function Node(i,j){
 	this.f = 0;
@@ -35,20 +35,44 @@ function Node(i,j){
 	this.neighbors = [];
 	this.prev;
 	this.wall = false;
+	this.diameter;
+	this.x;
+	this.y;
+	this.color = color(210);
 
 	if(random(1) < 0.3){// Change to increase more walls
 		this.wall = true;
+		this.color = color(0);
+	}
+	
+	this.onClicked = function() {
+		if(pointCount > 0){
+			var d = dist(mouseX,mouseY,this.x,this.y);
+			if(d < this.diameter/2){
+				if(pointCount == 2 ){
+					console.log("Start",this.i,this.j);
+					start = grid[this.i][this.j];
+					openSet.push(start);
+				}
+				if(pointCount == 1){
+					console.log("Finish",this.i,this.j);
+					end = grid[this.i][this.j];
+					end.wall = false;
+				}
+				pointCount--;
+			}
+		}
 	}
 
 	this.show = function(col) {
-		fill(col);
-		if(this.wall){
-			fill(0);
-		}
+		fill(this.color);
 		noStroke();
-		ellipse(this.i * w + w/2,this.j *h + h/2, w/2,h/2);
-		//rect(this.i * w,this.j * h, w-1,h-1);
+		this.x = this.i * w + w/2;
+		this.y = this.j *h + h/2;
+		this.diameter = w/2;
+		this.shape = ellipse(this.x,this.y, this.diameter,this.diameter);
 	}
+
 	this.addNeighbors = function() {
 		var i=this.i;
 		var j=this.j;
@@ -81,8 +105,16 @@ function Node(i,j){
 	}
 }
 
+function mousePressed() {
+	for(var i=0;i< cols; i++){
+		for(var j=0;j<rows;j++){
+			grid[i][j].onClicked();
+		}
+	}			
+}
+
 function setup() {
-	var canvas = createCanvas(600,600);
+	var canvas = createCanvas(800,800);
   	canvas.parent('sketch-holder');
 	w = width / cols;
 	h = height / rows;
@@ -102,17 +134,13 @@ function setup() {
 			grid[i][j].addNeighbors();
 		}
 	}
-	console.log(grid);
-	
-	start = grid[0][0];
-	end = grid[cols-1][rows-1]
-
-	openSet.push(start);
 }
 
 // P5 draw allows for continuous 
 // Pseudocode from https://en.wikipedia.org/wiki/A*_search_algorithm
-function draw() {
+// TODO openSet min heap
+//
+function aStar(){
 	if(openSet.length > 0) {
 		var lowest = 0;
 		for(var i=0;i<openSet.length;i++){ // Find Smallest Path aka f = g + h
@@ -120,7 +148,7 @@ function draw() {
 				lowest = i;
 			}
 		}
-		var current = openSet[lowest];
+		var current = openSet[lowest]; // current = openSet.getMin()
 		if(current === end){
 			noLoop(); // The noLoop() function causes draw() to only execute once
 			console.log("Done");
@@ -128,6 +156,7 @@ function draw() {
 		//Remove From openSet 
 		// TODO optimize
 		removeFromArray(openSet,current);
+		//
 		closedSet.push(current);
 
 		var neighbors = current.neighbors;
@@ -136,57 +165,65 @@ function draw() {
 
 			if (!closedSet.includes(neighbor) && !neighbor.wall){ // Not in closed set and not wall
 				var tentative_gScore = current.g + heruristic(neighbor,current);// Distance is always 1 in this case 
-				if(neighbor.g < tentative_gScore){
-					tentative_gScore = neighbor.g;	
-				}
 				if(!openSet.includes(neighbor)){
 					openSet.push(neighbor);
 				}
+				if(neighbor.g < tentative_gScore){
+					tentative_gScore = neighbor.g;		
+				}
+
 				neighbor.prev = current;
 				neighbor.g = tentative_gScore;
 				neighbor.h = heruristic(neighbor,end);
 				neighbor.f = neighbor.h + neighbor.g;
 			}
 		}
+		return current;
 	}// End Of A* 
 	else{
-		// No solutiion
-		return; 
+		// No Solution  
+		console.log("No Solution");
+		noLoop();
 	}
 
-	// Fill Grid 
-	//background(255);
+}
+function draw() {
+	var current;
+	// A Star Algorithm
+	if(start != -1 && end != -1){
+		current = aStar();
+	}
+	// Set Closed Set To Red
+	for(var i=0;i<closedSet.length;i++){
+		closedSet[i].color = color(255,0,0);
+	} 
+	// Set Open Set Color to Green
+	for(var i=0;i<openSet.length;i++){
+		openSet[i].color = color(0,255,0);
+	}
+	// Show Grid
 	for(var i=0;i< cols; i++){
 		for(var j=0;j<rows;j++){
-			grid[i][j].show(color(255));
+			grid[i][j].show();
 		}
-	}			
-	for(var i=0;i<closedSet.length;i++){
-		closedSet[i].show(color(255,0,0));
-	} 
-	for(var i=0;i<openSet.length;i++){
-		openSet[i].show(color(0,255,0));
+	}		
+	// Draw Path
+	if(typeof current != 'undefined'){
+		path = [];
+		var temp = current;
+		path.push(temp)
+		while(temp.prev){
+			path.push(temp.prev);
+			temp = temp.prev;
+		}
+		beginShape();
+		noFill();
+		stroke(255,0,255);
+		strokeWeight(w/2);
+		for(var i=0;i<path.length;i++){
+			vertex(path[i].i * w + w/2, path[i].j * h + h/2);
+		}
+		endShape();
 	}
-
-	path = [];
-	var temp = current;
-	path.push(temp)
-	while(temp.prev){
-		path.push(temp.prev);
-		temp = temp.prev;
-	}
-	/*
-	for(var i=0;i<path.length;i++){
-		path[i].show(color(0,0,255));
-	}
-	*/
-
-	beginShape();
-	noFill();
-	stroke(255,0,255);
-	strokeWeight(w/2);
-	for(var i=0;i<path.length;i++){
-		vertex(path[i].i * w + w/2, path[i].j * h + h/2);
-	}
-	endShape();
 }
+
